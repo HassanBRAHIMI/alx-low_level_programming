b374bcfdb9857f3b273bccf1e7d56264a0909684
Hassan <brahimihassan054@gmail.com>
2023-10-05 13:40:41 -0700

    added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main - entry point
+/**
+ * main -Entry point
  *
- * return: 0 in succes
+ * Return: 0 in succes
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
