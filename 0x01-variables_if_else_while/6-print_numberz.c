ea44ad5994eefda85d0f50d8e352eb3ec209dba5
HassanBRAHIMI <brahimihassan054@gmail.com>
2023-10-06 23:42:40 +0100

    added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 succes
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
+
