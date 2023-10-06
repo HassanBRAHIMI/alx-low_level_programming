23540af68aa476c7b84dcfb1bdbfb6403c8e3f4c
Hassan <brahimihassan054@gmail.com>
2023-10-06 05:12:27 -0700

    added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 succes
+ */
+int main(void)
+{
+	char a = 97;
+
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
