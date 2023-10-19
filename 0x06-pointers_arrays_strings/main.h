cacdda3486fa20bc00c595dc3601202f1006a50f
HassanBRAHIMI <brahimihassan054@gmail.com>
2023-10-19 23:22:15 +0100

    added

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 char *_strncat(char *dest, char *src, int n);
+char *cap_string(char *s);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *s);
 void reverse_array(int *a, int n);
