1f51ade30bbf78ce22ea82f6381cc1fd58330d0a
HassanBRAHIMI <brahimihassan054@gmail.com>
2023-10-19 23:14:54 +0100

    added

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+/**
+ * _strcmp - man the function strcmp
+ * @s1:string 1
+ * @s2:string 2
+ * Return: The integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
