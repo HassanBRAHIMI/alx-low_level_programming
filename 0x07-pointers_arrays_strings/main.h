e7633f24db83e971eaccca373e41d25fb2df152f
HassanBRAHIMI <brahimihassan054@gmail.com>
2023-10-23 20:34:38 +0100

    added

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 #include <stddef.h>
 char *_memset(char *s, char b, unsigned int n);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 int _putchar(char c);
 char *_strstr(char *haystack, char *needle);
 char *_memcpy(char *dest, char *src, unsigned int n);
