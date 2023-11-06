865eddf75156469de50ceb338dddc01176fd93c4
HassanBRAHIMI <brahimihassan054@gmail.com>
2023-11-06 20:52:48 +0100

    added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog truct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Desceiption: structure description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
