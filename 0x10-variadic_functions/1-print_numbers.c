98e74309f2532df9f3f8df192bdeff6e415d8107
HassanBRAHIMI <brahimihassan054@gmail.com>
2023-11-10 23:02:37 +0100

    added

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,32 +1,28 @@
 #include "variadic_functions.h"
+
 /**
- * print_numbers - prints numbers
- * @separator: separator
- * @n : number of arguments
- *
- * Return: void
+ * print_numbers - prints numbers followed by a new line
+ * @n: number of parameter
+ * @separator: separator between two numbers
+ * Return: Always 0
  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list args;
+	va_list ap;
 
-	va_start(args, n);
-	if (!separator)
+	va_start(ap, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(args, int));
-		}
-	}
-	else
-	{
-	for (i = 1; i < n; i++)
-	{
-		printf("%d%s", va_arg(args, int), separator);
-	}
-	printf("%d", va_arg(args, int));
+		printf("%d", va_arg(ap, int));
+		if (i < n - 1)
+			printf("%s", separator);
 	}
 	printf("\n");
-	va_end(args);
+	va_end(ap);
 }
