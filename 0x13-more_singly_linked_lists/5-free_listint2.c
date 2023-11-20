2cf9e48f91a6e4dd17bf8d1aadc5b2ba65d16595
HassanBRAHIMI <brahimihassan054@gmail.com>
2023-11-20 21:56:59 +0100

    added

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,14 +5,15 @@
  *
  * Return: void
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head)
+	while (*head)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = *head->next;
+		free(*head);
+		*head = temp;
 	}
+	head == NULL;
 }
