6aad9cc9f36af449a718824c093a1a24cca57f8f
HassanBRAHIMI <brahimihassan054@gmail.com>
2023-11-20 21:54:43 +0100

    added

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint list
+ * @head: head
+ *
+ * Return: void
+ */
+void free_listint2(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
