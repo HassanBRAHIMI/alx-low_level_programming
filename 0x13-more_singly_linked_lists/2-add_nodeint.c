c988e87e912382e50fa77129df470a8c807719a7
HassanBRAHIMI <brahimihassan054@gmail.com>
2023-11-20 18:49:06 +0100

    added

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,15 +4,15 @@
  * @head: the adress of the head
  * @n: the int n
  *
- * Retutn: the adress of the new element
+ * Return: the adress of the new element
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *temp;
 
 	temp = malloc(sizeof(listint_t));
-	if(!temp)
-		return(NULL);
+	if (!temp)
+		return (NULL);
 	temp->n = (int)n;
 	temp->next = *head;
 	*head = temp;
