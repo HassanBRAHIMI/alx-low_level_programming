78830d68f6a46fa5a778f176f239f811b508ee16
HassanBRAHIMI <brahimihassan054@gmail.com>
2023-12-24 12:03:59 +0100

    added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int o, w, content_len;
 
-	if (!text_content)
+	if (!text_content || !filename)
 		return (-1);
 	o = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (o < 0)
