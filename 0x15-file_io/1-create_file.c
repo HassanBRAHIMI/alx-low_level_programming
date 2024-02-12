1f5d3b3c13eb3ffe7b26d4de807478699bb9bf51
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-02-12 21:43:35 +0100

    added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (o == -1)
 		return (-1);
 	content_len = 0;
-	if(text_content)
+	if (text_content)
 	{
 		while (text_content[content_len])
 		content_len++;
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,11 @@
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: file name
+ * @text_content: text content
+ *
+ * Return: 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
