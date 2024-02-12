c0b747103a6949cbd467b584b578b15b5a01705d
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-02-12 16:31:56 +0100

    added

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,28 +16,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
 	char *buffer;
+	ssize_t bytes_read;
+	ssize_t bytes_write;
+	ssize_t fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	buffer = (char *)malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+	bytes_read = read(fd, buffer, letters);
+	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_write == -1 || bytes_read == -1 ||
+			bytes_read != bytes_write)
 	{
 		free(buffer);
 		return (0);
 	}
-
 	free(buffer);
-	close(o);
-
-	return (w);
+	close(fd);
+	return (bytes_write);
 }
