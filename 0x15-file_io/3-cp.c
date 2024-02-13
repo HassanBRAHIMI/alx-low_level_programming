17825baea17e58ab4b9a247c91e8539358edece8
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-02-13 17:26:44 +0100

    added

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,24 +3,23 @@
  * @ac: arguments count
  * @av: arguments vector
  *
- *Return: 97/98/99/100 in error 
+ *Return: 97/98/99/100 in error
 */
 #include "main.h"
 int main(int ac, char **av)
 {
-char *file_from;
-char *file_to;
-char buff[1024];
+char *file_from, *file_to, buff[1024];
 int file_to_fd, readed_f_filefrom, file_from_fd, written, to, from;
 
 if (ac != 3)
 {
 dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
-exit (97);
+exit(97);
 }
 file_from = av[1];
 file_to = av[2];
-file_to_fd = open(file_to, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+file_to_fd = open(file_to, O_CREAT | O_RDWR | O_TRUNC,
+	S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (file_to_fd == -1)
 {
 dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't write to file", file_to);
@@ -30,7 +29,7 @@ file_from_fd = open(file_from, O_RDONLY);
 if (file_from_fd == -1)
 {
 dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't read from file", file_from);
-exit(98); 
+exit(98);
 }
 readed_f_filefrom = 1024;
 while (readed_f_filefrom == 1024)
@@ -44,7 +43,7 @@ exit(98);
 written = write(file_to_fd, buff, readed_f_filefrom);
 if (written == -1)
 {
-dprintf(STDERR_FILENO,"%s %s\n", "Error: Can't write to", file_to);
+dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't write to", file_to);
 exit(99);
 }
 }
