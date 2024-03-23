da243cf8e4f8987b5be176a8664ad2f46fb58926
Hassan BRAHIMI <107075554+HassanBRAHIMI@users.noreply.github.com>
2024-03-23 17:27:25 +0000

    added

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,5 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->prev = NULL;
 	new_node->next = *head;
 	*head = new_node;
+	if (new_node->next != NULL)
+		(new_node->next)->prev = new;
 	return (new_node);
 }
