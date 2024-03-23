713b66b27065b995cbfb6fde0fd7ab5ab6e6a8fa
Hassan BRAHIMI <107075554+HassanBRAHIMI@users.noreply.github.com>
2024-03-23 22:52:28 +0000

    added

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,13 +21,21 @@ size_t n;
 	return (n);
 }
 
+/**
+ * insert_dnodeint_at_index - long shit
+ * @h: the head
+ * @idx: index
+ * @n: n
+ *
+ * Return: the return value
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
 	dlistint_t *temp;
 	dlistint_t *holder;
 	dlistint_t *new_node;
-	
+
 	new_node = malloc(sizeof(dlistint_t *));
 	if (!new_node)
 		return (NULL);
@@ -37,27 +45,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp = *h;
 	i = 0;
 	if (idx > dlistint_len(*h))
+	{
+		free(new_node);
 		return (NULL);
+	}
+	if (idx == dlistint_len(*h))
+		return (add_dnodeint_end(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	if (!*h)
 	{
 		*h = new_node;
 		return (new_node);
 	}
-	while(temp && i < idx)
+	while (temp && i < idx)
 	{
 		temp = temp->next;
 		i++;
 	}
-	/*holder = temp;
-	temp->next = new_node;
-	new_node->prev = temp;
-	new_node->next = holder->next;
-	(holder->next)->prev = new_node;*/
 	holder = temp->prev;
-    if (holder)
-        holder->next = new_node;
-    new_node->prev = holder;
-    new_node->next = temp;
-    temp->prev = new_node;
+	if (holder)
+		holder->next = new_node;
+	new_node->prev = holder;
+	new_node->next = temp;
+	temp->prev = new_node;
 	return (new_node);
 }
